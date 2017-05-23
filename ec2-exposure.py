891cf3a04f05bad70e9775771755a67b09730216
h4m24 <eng.hfaouri@gmail.com>
2017-05-23 16:09:48 +0200

    made output more readable

--- ec2-exposure.py
+++ ec2-exposure.py
@@ -19,11 +19,14 @@ for Ec2Instance in client.describe_instances()['Reservations']:
         for Cidrs in client.describe_security_groups(GroupIds=InstanceSecurityGroupList)['SecurityGroups'][0]['IpPermissions'][0]['IpRanges']:
             if Cidrs['CidrIp'] == '0.0.0.0/0':
 
-                print("Instance info:")
-                print(InstanceSecurityGroupList)
+                for TagDocument in Ec2Instance['Instances'][0]['Tags']:
+                    if TagDocument['Key'] == 'Name':
+                        print(TagDocument['Value'])
                 print(Ec2Instance['Instances'][0]['PublicDnsName'])
                 print(Ec2Instance['Instances'][0]['InstanceId'])
-                print(Ec2Instance['Instances'][0]['Tags'])
+                # print(InstanceSecurityGroupList)
+
+                # print(Ec2Instance['Instances'][0]['Tags'])
                 for Cidrs in client.describe_security_groups(GroupIds=InstanceSecurityGroupList)['SecurityGroups'][0]['IpPermissions'][0]['IpRanges']:
                     print(Cidrs['CidrIp'])
                 print("\n")
