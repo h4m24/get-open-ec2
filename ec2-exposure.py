3329d951c19781b5c2e9aa9efb48de72ba4622cf
h4m24 <eng.hfaouri@gmail.com>
2017-05-23 17:27:47 +0200

    output is a table now

--- ec2-exposure.py
+++ ec2-exposure.py
@@ -1,9 +1,10 @@
 import boto3
+from prettytable import PrettyTable
 
 InstanceSecurityGroupList =[]
 client = boto3.client('ec2')
 
-
+report = PrettyTable(['Name', 'publicDNS', 'instanceID'])
 
 for Ec2Instance in client.describe_instances()['Reservations']:
 
@@ -21,14 +22,11 @@ for Ec2Instance in client.describe_instances()['Reservations']:
 
                 for TagDocument in Ec2Instance['Instances'][0]['Tags']:
                     if TagDocument['Key'] == 'Name':
-                        print(TagDocument['Value'])
-                print(Ec2Instance['Instances'][0]['PublicDnsName'])
-                print(Ec2Instance['Instances'][0]['InstanceId'])
-                # print(InstanceSecurityGroupList)
+                        Ec2Name = TagDocument['Value']
 
-                # print(Ec2Instance['Instances'][0]['Tags'])
-                for Cidrs in client.describe_security_groups(GroupIds=InstanceSecurityGroupList)['SecurityGroups'][0]['IpPermissions'][0]['IpRanges']:
-                    print(Cidrs['CidrIp'])
-                print("\n")
-                print("\n")
+                report.add_row([Ec2Name, Ec2Instance['Instances'][0]['PublicDnsName'],Ec2Instance['Instances'][0]['InstanceId']])
+
                 continue
+
+print(" following instances with 0.0.0.0/0 in security group setup:")
+print(report)
